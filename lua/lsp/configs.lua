123d232c87601a55bfa427d60498a58f1f10d284
Thilina Lakshan <thilinalakshanmail@gmail.com>
2022-02-10 07:14:39 +0530

    configure lsp

---
+++ lua/lsp/configs.lua
@@ -0,0 +1,125 @@
+-- LSP Configurations
+
+-- Generate absolute path to lsp servers
+local lsp_dir = vim.fn.stdpath('data') .. '/lsp_servers'
+local function lsp_path(relative_path)
+  return vim.fn.expand(lsp_dir .. '/' .. relative_path)
+end
+
+-- root dir pattern generator
+local function root_dirs(patterns)
+    return function (fname)
+        local util = require('lspconfig').util
+        return util.root_pattern(unpack(patterns))(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
+    end
+end
+
+-------------------------------------------------------------------------------------------------------------
+local lsp_configs = {}
+
+-- Lua ------------------------------------------------------------------------------------------------------
+lsp_configs.lua = {
+        name = 'sumneko_lua',
+        cmd = {lsp_path('sumneko_lua/extension/server/bin/lua-language-server')};
+        settings = {
+            Lua = {
+                runtime = {
+                    version = 'LuaJIT',
+                    path = vim.split(package.path, ';'),
+                },
+                diagnostics = {
+                    globals = {'vim'},
+                },
+                workspace = {
+                    library = {
+                        [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+                        [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+                    },
+                },
+                telemetry = {
+                    enable = false,
+                },
+            },
+        },
+}
+
+-- Python ---------------------------------------------------------------------------------------------------
+lsp_configs.python = {
+    name = 'pylsp',
+    cmd = {lsp_path('pylsp/venv/bin/pylsp')};
+    config = {
+        configurationSources = { "flake8" },
+        plugins = {
+            pyls_mypy = {
+                enabled = true,
+                live_mode = true
+            },
+            flake8 = {
+                enabled = true,
+            },
+            pydocstyle = {
+                enabled = true
+            }
+        }
+    },
+    root_dir = root_dirs({
+        'pyproject.toml',
+        'setup.py',
+        'setup.cfg',
+        'requirements.txt',
+        'Pipfile',
+        'main.py'
+    }),
+
+}
+
+-- Go lang --------------------------------------------------------------------------------------------------
+lsp_configs.go = {
+    name = 'gopls',
+    cmd = {lsp_path('go/gopls')},
+    config = {},
+    root_dir = root_dirs({
+        'go.mod',
+        '.git',
+        'main.go',
+        'test.go'
+    }),
+
+}
+
+-- Typescript, Javascript -----------------------------------------------------------------------------------
+lsp_configs.typescript = {
+    name = 'tsserver',
+    cmd = {'node', lsp_path('tsserver/node_modules/typescript-language-server/lib/cli.js'), '--stdio'},
+    config = {},
+    init_options = { hostInfo = "neovim" },
+    root_dir = root_dirs({
+        'package.json',
+        'tsconfig.json',
+        '.git',
+        'main.ts',
+        'main.js',
+        'index.ts',
+        'index.js',
+    })
+}
+
+-- Angular --------------------------------------------------------------------------------------------------
+lsp_configs.angular = {
+    name = 'angularls',
+    cmd = {lsp_path('angularls/node_modules/@angular/language-server/bin/ngserver'), '--stdio'},
+    config = {},
+    root_dir = root_dirs({
+        'angular.json',
+        'package.json',
+        'tsconfig.json',
+        '.git',
+    })
+}
+-------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------
+return {
+    configs = lsp_configs
+}
+
--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,125 +1,19 @@
--- LSP Configurations
+local lsp_installer_servers = require('nvim-lsp-installer.servers')
+local configs = require('lsp/configs').configs
 
--- Generate absolute path to lsp servers
-local lsp_dir = vim.fn.stdpath('data') .. '/lsp_servers'
-local function lsp_path(relative_path)
-  return vim.fn.expand(lsp_dir .. '/' .. relative_path)
-end
-
--- root dir pattern generator
-local function root_dirs(patterns)
-    return function (fname)
-        local util = require('lspconfig').util
-        return util.root_pattern(unpack(patterns))(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
+for _, config in pairs(configs) do
+    local server_available, server = lsp_installer_servers.get_server(config.name)
+    if server_available then
+        server:on_ready(function ()
+            local opts = {}
+            opts.root_dir = config.root_dir
+            opts.config = config.config
+            print(string.format("%s: %s, cmd: %s", config.name, config.root_dir, server.cmd))
+            server:setup(opts)
+        end)
+        -- if not server:is_installed() then
+        --     server:install()
+        -- end
     end
 end
-
--------------------------------------------------------------------------------------------------------------
-local lsp_configs = {}
-
--- Lua ------------------------------------------------------------------------------------------------------
-lsp_configs.lua = {
-        name = 'sumneko_lua',
-        cmd = {lsp_path('sumneko_lua/extension/server/bin/lua-language-server')};
-        settings = {
-            Lua = {
-                runtime = {
-                    version = 'LuaJIT',
-                    path = vim.split(package.path, ';'),
-                },
-                diagnostics = {
-                    globals = {'vim'},
-                },
-                workspace = {
-                    library = {
-                        [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-                        [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-                    },
-                },
-                telemetry = {
-                    enable = false,
-                },
-            },
-        },
-}
-
--- Python ---------------------------------------------------------------------------------------------------
-lsp_configs.python = {
-    name = 'pylsp',
-    cmd = {lsp_path('pylsp/venv/bin/pylsp')};
-    config = {
-        configurationSources = { "flake8" },
-        plugins = {
-            pyls_mypy = {
-                enabled = true,
-                live_mode = true
-            },
-            flake8 = {
-                enabled = true,
-            },
-            pydocstyle = {
-                enabled = true
-            }
-        }
-    },
-    root_dir = root_dirs({
-        'pyproject.toml',
-        'setup.py',
-        'setup.cfg',
-        'requirements.txt',
-        'Pipfile',
-        'main.py'
-    }),
-
-}
-
--- Go lang --------------------------------------------------------------------------------------------------
-lsp_configs.go = {
-    name = 'gopls',
-    cmd = {lsp_path('go/gopls')},
-    config = {},
-    root_dir = root_dirs({
-        'go.mod',
-        '.git',
-        'main.go',
-        'test.go'
-    }),
-
-}
-
--- Typescript, Javascript -----------------------------------------------------------------------------------
-lsp_configs.typescript = {
-    name = 'tsserver',
-    cmd = {'node', lsp_path('tsserver/node_modules/typescript-language-server/lib/cli.js'), '--stdio'},
-    config = {},
-    init_options = { hostInfo = "neovim" },
-    root_dir = root_dirs({
-        'package.json',
-        'tsconfig.json',
-        '.git',
-        'main.ts',
-        'main.js',
-        'index.ts',
-        'index.js',
-    })
-}
-
--- Angular --------------------------------------------------------------------------------------------------
-lsp_configs.angular = {
-    name = 'angularls',
-    cmd = {lsp_path('angularls/node_modules/@angular/language-server/bin/ngserver'), '--stdio'},
-    config = {},
-    root_dir = root_dirs({
-        'angular.json',
-        'package.json',
-        'tsconfig.json',
-        '.git',
-    })
-}
--------------------------------------------------------------------------------------------------------------
--------------------------------------------------------------------------------------------------------------
--------------------------------------------------------------------------------------------------------------
-return {
-    configs = lsp_configs
-}
 
