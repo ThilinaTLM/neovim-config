c153acfe20717b5be9b999cda0db876b8a09d811
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2022-06-30 16:21:29 +0530

    minor change

--- init.lua
+++ init.lua
@@ -29,7 +29,7 @@ local settings = {
             vim.g.material_style = "darker"
         end,
         post = function ()
-            vim.cmd ":highlight NormalFloat guibg='Black' guifg=NONE<CR>"
+            vim.cmd ":highlight NormalFloat guibg='#111111' guifg=NONE<CR>"
         end,
         colorscheme = 'material',
     },
@@ -110,16 +110,15 @@ augroup END
 
 vim.cmd [[set updatetime=700]]
 vim.api.nvim_create_augroup('lsp-hold-highlight', {clear = true})
--- vim.api.nvim_create_autocmd('CursorHold', {
---     callback = function ()
---         vim.lsp.buf.document_highlight()
---     end,
---     group = 'lsp-hold-highlight',
--- })
--- vim.api.nvim_create_autocmd('CursorMoved', {
---     callback = function ()
---         vim.lsp.buf.clear_references()
---     end,
---     group = 'lsp-hold-highlight',
--- })
---
+vim.api.nvim_create_autocmd('CursorHold', {
+    callback = function ()
+        -- vim.lsp.buf.document_highlight()
+    end,
+    group = 'lsp-hold-highlight',
+})
+vim.api.nvim_create_autocmd('CursorMoved', {
+    callback = function ()
+        -- vim.lsp.buf.clear_references()
+    end,
+    group = 'lsp-hold-highlight',
+})
